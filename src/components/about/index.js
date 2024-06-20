f9997d41e76ea98e589f980df86907dfe2ea1d32
Gek <gakidogeck@gmail.com>
2024-06-19 18:11:10 -0700

    cube

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -2,8 +2,9 @@ import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
 import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCss3, faGit, faGitAlt, faHtml5, faJsSquare, faPython, faReact } from '@fortawesome/free-brands-svg-icons';
-import { faFlask, faFlaskVial } from '@fortawesome/free-solid-svg-icons';
+import { faCss3, faGitAlt, faHtml5, faJsSquare, faPython, faReact } from '@fortawesome/free-brands-svg-icons';
+import Loader from 'react-loaders';
+import { faFlask } from '@fortawesome/free-solid-svg-icons';
 const About = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
 
@@ -36,15 +37,26 @@ const About = () => {
                 <div className='cubespinner'>
                     <div className='face1'>
                         <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-                        <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-                        <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-                        <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-                        <FontAwesomeIcon icon={faPython} color="#CC6699" />
-                        <FontAwesomeIcon icon={faFlask} color="#28A4D9" />
-                        <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+                    </div>
+                    <div className='face2'>
+                    <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+                    </div>
+                    <div className='face3'>
+                    <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+                    </div>
+                    <div className='face4'>
+                    <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+                    </div>
+                    <div className='face5'>
+                    <FontAwesomeIcon icon={faPython} color="#CC6699" />
+                    </div>
+                    <div className='face6'>
+                    <FontAwesomeIcon icon={faFlask} color="#28A4D9" />
                     </div>
                 </div>
             </div>
+
+            <Loader type="pacman" />
         </div>
     )
 }
